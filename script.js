1dfd567ef7e00949f2f247da84e9f51e9d2bbe79
hafsaleo <hafsaaltiri@gmail.com>
2025-10-19 12:37:06 +0300

    Correct typos in script.js for form handling
    Fix typos in form submission alerts and event handling.

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("contactform").onsubmit=function(){alert("your ruqierd has been submit");
+return fales;
+ }; 
+
+document.getElementsByTegName("tr")[1].onsubmit=function(){alert("candles sold out");
+};                                                    
